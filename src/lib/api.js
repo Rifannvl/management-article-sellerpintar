c3818087ea46c68cbb240a21f9ad6dfe2c0e8e6e
Rifannvl <riffanafendi@gmail.com>
2025-05-08 22:38:06 +0700

    membuat api.js untuk token dan api

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,3 +7,15 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+// Tambahkan interceptor untuk menyisipkan token Authorization
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    console.log("Token dari interceptor:", token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
